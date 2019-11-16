d20b3600064a0c4afcf016550fe2b9ec436f6bcb
antoniofpereir <antoniofpereir@gmail.com>
2019-11-16 12:12:21 +0000

    Initial dependencies installed. Webpack and Babel configuration.

---
+++ my-code/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./dist/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [path.resolve("./src"), path.resolve("./node_modules")]
+  }
+};
